eb54e089836554a43fa6b5e3f969d4c9d12635af
Maciek <maciejfrs@gmail.com>
2023-03-26 21:21:31 +0100

    Date added

--- lab3/script.js
+++ lab3/script.js
@@ -9,4 +9,25 @@ const addTodo = (todo) => {
 
 list.onclick = (event) => {
     event.target.classList.toggle('checked');
+    const label = document.createElement('label');
+
+    if (event.target.classList.contains('checked')) {
+        date = new Date();
+        minutes = date.getMinutes();
+        if (minutes <= 9) {
+            minutes = '0' + minutes
+        }
+        hours = date.getHours();
+        if (hours <= 9) {
+            hours = '0' + hours
+        }
+
+        label.innerText = `${date.getFullYear()}/${date.getDate()}/${date.getMonth() + 1} ${hours}:${minutes}`;
+        label.classList.add('date-label');
+        event.target.appendChild(label);
+    } else {
+        event.target.classList.remove('label');
+        const dateLabel = event.target.querySelector('label');
+        event.target.removeChild(dateLabel);
+    }
 }
