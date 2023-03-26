325349c7ac68798150a58a70b9147def40214571
Maciek <maciejfrs@gmail.com>
2023-03-26 19:23:36 +0100

    Graying and crossing out todo-item added

--- lab3/script.js
+++ lab3/script.js
@@ -1,8 +1,12 @@
+const list = document.getElementById('todos-list');
 const addTodo = (todo) => {
     if (todo) {
-        const list = document.getElementById('todos-list');
         const entry = document.createElement('li');
         entry.appendChild(document.createTextNode(todo));
         list.appendChild(entry);
     }
+}
+
+list.onclick = (event) => {
+    event.target.classList.toggle('checked');
 }
