213b5498213e47ef01980f079aad109f28f408a7
Maciek <maciejfrs@gmail.com>
2023-03-31 14:57:59 +0100

    Confirmation modal added

--- lab3/script.js
+++ lab3/script.js
@@ -1,6 +1,7 @@
 "use strict"
 const list = document.getElementById('todos-list');
 let lastRemovedTodo = null;
+let todoToRemove = null;
 const addTodo = (todo) => {
     if (todo) {
         const entry = document.createElement('li');
@@ -8,9 +9,11 @@ const addTodo = (todo) => {
         entry.appendChild(document.createTextNode(todo));
         list.appendChild(entry);
         $('#todos-list').on('click', '.delete-button', (event) => {
+            $('#modal').css("display", "block");
             lastRemovedTodo = todo;
-            $(event.currentTarget).parent().remove();
+            todoToRemove = event.currentTarget;
         });
+
     }
 }
 
@@ -21,28 +24,42 @@ const restoreTodo = () => {
     }
 }
 
-list.onclick = (event) => {
-    event.target.classList.toggle('checked');
-    const label = document.createElement('label');
+$('#todos-list').on('click', 'li', (event) => {
+    if (!event.target.classList.contains('delete-button')) {
 
-    if (event.target.classList.contains('checked')) {
-        let date = new Date();
-        let minutes = date.getMinutes();
-        if (minutes <= 9) {
-            minutes = '0' + minutes
-        }
-        let hours = date.getHours();
-        if (hours <= 9) {
-            hours = '0' + hours
-        }
+        event.target.classList.toggle('checked');
+        const label = document.createElement('label');
 
-        label.innerText = `${date.getFullYear()}/${date.getDate()}/${date.getMonth() + 1} ${hours}:${minutes}`;
-        label.classList.add('date-label');
-        event.target.appendChild(label);
-    } else {
-        event.target.classList.remove('label');
-        const dateLabel = event.target.querySelector('label');
-        event.target.removeChild(dateLabel);
-        label.classList.remove('date-label');
+        if (event.target.classList.contains('checked')) {
+            let date = new Date();
+            let minutes = date.getMinutes();
+            if (minutes <= 9) {
+                minutes = '0' + minutes
+            }
+            let hours = date.getHours();
+            if (hours <= 9) {
+                hours = '0' + hours
+            }
+
+            label.innerText = `${date.getFullYear()}/${date.getDate()}/${date.getMonth() + 1} ${hours}:${minutes}`;
+            label.classList.add('date-label');
+            event.target.appendChild(label);
+        } else {
+            event.target.classList.remove('label');
+            const dateLabel = event.target.querySelector('label');
+            event.target.removeChild(dateLabel);
+            label.classList.remove('date-label');
+        }
     }
+});
+
+const cancelRemovingTodo = () => {
+    $('#modal').css("display", "none");
+}
+
+const confirmRemovingTodo = () => {
+    if (todoToRemove) {
+        $(todoToRemove).parent().remove();
+    }
+    $('#modal').css("display", "none");
 }
