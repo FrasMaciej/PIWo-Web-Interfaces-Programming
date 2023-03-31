0fb173a25a21b0669c305ce44fa7a5cce97206f5
Maciek <maciejfrs@gmail.com>
2023-03-31 13:04:36 +0100

    Restore todo functionality added

--- lab3/script.js
+++ lab3/script.js
@@ -1,14 +1,23 @@
 "use strict"
 const list = document.getElementById('todos-list');
+let lastRemovedTodo = null;
 const addTodo = (todo) => {
     if (todo) {
         const entry = document.createElement('li');
         $(entry).append('<button class="delete-button">X</button>');
-        $('#todos-list').on('click', '.delete-button', (event) => {
-            $(event.currentTarget).parent().remove();
-        });
         entry.appendChild(document.createTextNode(todo));
         list.appendChild(entry);
+        $('#todos-list').on('click', '.delete-button', (event) => {
+            lastRemovedTodo = todo;
+            $(event.currentTarget).parent().remove();
+        });
+    }
+}
+
+const restoreTodo = () => {
+    if (lastRemovedTodo) {
+        addTodo(lastRemovedTodo);
+        lastRemovedTodo = null;
     }
 }
 
@@ -17,12 +26,12 @@ list.onclick = (event) => {
     const label = document.createElement('label');
 
     if (event.target.classList.contains('checked')) {
-        const date = new Date();
-        const minutes = date.getMinutes();
+        let date = new Date();
+        let minutes = date.getMinutes();
         if (minutes <= 9) {
             minutes = '0' + minutes
         }
-        const hours = date.getHours();
+        let hours = date.getHours();
         if (hours <= 9) {
             hours = '0' + hours
         }
@@ -34,5 +43,6 @@ list.onclick = (event) => {
         event.target.classList.remove('label');
         const dateLabel = event.target.querySelector('label');
         event.target.removeChild(dateLabel);
+        label.classList.remove('date-label');
     }
 }
